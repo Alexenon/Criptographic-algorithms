9ddfc630928c196a3cb50faac9fbf55826170829
Alex Xenon <b3haply@gmail.com>
2021-12-06 13:03:33 +0200

    First add from batch

---
+++ VGN seed algorithm/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "vgm.c" 
+
+
+// TIME IS SET TO 0 ALWAYS
+
+int main(int argc, char *argv[]){
+
+    char *prg=argv[0];
+    char *opt=argv[1];
+    char *msg=argv[2];
+
+    if(argc==1) {
+        fprintf(stderr, "%s: no option and massage\n", prg);
+        exit(3);
+    }
+    if(argc==2) {
+        if(opt[0] != '-')
+            fprintf(stderr, "option is missing\n");
+        else
+            fprintf(stderr, "message is missing\n");
+        exit(2);
+    }
+    if(argc==3){
+        if(strcmp(opt,"-Encr")==0)
+            VGM_encrypt_seed(msg);
+        else if(strcmp(opt,"-Decr")==0)
+            VGM_decrypt_seed(msg);
+        // else if(strcmp(opt,"-Encrp")==0)
+        //     CSK_encrypt_plus(msg);
+        else{
+            fprintf(stderr, "%s: illegal option: %s\n", prg, opt);
+            exit(1);
+        }
+    }
+    if(argc>3){
+        fprintf(stderr, "%s: too many args\n", prg);
+        exit(4);
+    }
+    exit(0);
+}
---
+++ VGN seed algorithm/vgm.c
@@ -0,0 +1,81 @@
+#ifndef VGM_C
+#define VGM_C
+#include <stdio.h>
+#include "vars.c"
+
+extern int tms;
+
+void VGM_encrypt_seed(char msg[])
+{
+    char enc[25], key[25];
+    int i, j, k, m, m1, m2;
+    int seed;
+
+    printf("key: ");
+    scanf("%s", key);
+    seed = time(NULL);
+    k=0;
+    for(i=0; i<strlen(msg); i++) {
+        for(j=0; j<strlen(letters); j++) {
+            if(msg[i] == letters[j])
+                m1 = j;
+            if(key[k] == letters[j])
+                m2 = j;
+        }
+        m = (m1+m2+seed) % strlen(letters);
+        enc[i] = letters[m];
+
+        if(k == strlen(key)-1)
+            k = 0;
+        else
+            k+=1;
+    }
+
+    printf("encrypt: ");
+        for(i=0; i<strlen(msg); i++)
+            printf("%c", enc[i]);
+    printf("\n");
+    printf("seed: %d\n", seed);
+    tms = seed;
+}
+
+
+void VGM_decrypt_seed(char msg[])
+{
+    char enc[25], key[25];
+    int i, j, k, m, m1, m2;
+
+    printf("Seed: %d\n", tms);
+
+    printf("key: ");
+    scanf("%s", key);
+
+
+    k=0;
+    for(i=0; i<strlen(msg); i++) {
+        for(j=0; j<strlen(letters); j++) {
+            if(msg[i] == letters[j])
+                m1 = j;
+            if(key[k] == letters[j])
+                m2 = j;
+        }
+        
+        /* Here is the magic */
+        m = (strlen(letters) + m1 - m2 - tms) % strlen(letters) + 4;
+        if(m>=26) 
+            m=0;
+        enc[i] = letters[m];
+
+        if(k == strlen(key)-1)
+            k = 0;
+        else
+            k+=1;
+    }
+
+    printf("decrypt: ");
+        for(i=0; i<strlen(msg); i++)
+            printf("%c", enc[i]);
+    printf("\n");
+}
+
+#endif
